76613041befac061512b6dbb2623cf229b605282
Mohamed Mujahid <Mohamed.mojahead@gmail.com>
2026-04-14 23:23:54 +0200

    config eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+import antfu from "@antfu/eslint-config"
+export default withNuxt(
+  antfu({
+    type: "app",
+    typescript: true,
+    vue: true,
+    formatters: true,
+    stylistic: {
+      indent: 4,
+      semi: true,
+      quotes: "double"
+    }
+  }, {
+    rules: {
+      "ts/consistent-type-definitions": ["error", "type"],
+      "no-console": ["warn"],
+      "antfu/no-top-level-await": ["off"],
+      "node/prefer-global/process": ["off"],
+      "node/no-process-env": ["error"],
+      "perfectionist/sort-imports": ["error", { tsconfig: { rootDir: "." } }],
+      "unicorn/filename-case": ["error", {
+        case: "kebabCase",
+        ignore: ["README.md"]
+      }]
+    }
+  })
+)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint'],
+  eslint: {
+    config: {
+      standalone: false
+    }
+  },
+  vite: {
+    optimizeDeps: {
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit',
+      ]
+    }
+  }
+
 })
