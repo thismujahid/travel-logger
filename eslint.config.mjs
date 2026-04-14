b656502bc63b525182dc9b9c052baef672dfcefb
Mohamed Mujahid <Mohamed.mojahead@gmail.com>
2026-04-14 23:33:25 +0200

    push linted files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,29 @@
+import antfu from "@antfu/eslint-config";
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
-import antfu from "@antfu/eslint-config"
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
 export default withNuxt(
-  antfu({
-    type: "app",
-    typescript: true,
-    vue: true,
-    formatters: true,
-    stylistic: {
-      indent: 4,
-      semi: true,
-      quotes: 'double'
-    }
-  }, {
-    rules: {
-      "ts/consistent-type-definitions": ["error", "type"],
-      "no-console": ["warn"],
-      "antfu/no-top-level-await": ["off"],
-      "node/prefer-global/process": ["off"],
-      "node/no-process-env": ["error"],
-      "unicorn/filename-case": ["error", {
-        case: "kebabCase",
-        ignore: ["README.md"]
-      }]
-    }
-  })
-)
+    antfu({
+        type: "app",
+        typescript: true,
+        vue: true,
+        formatters: true,
+        stylistic: {
+            indent: 4,
+            semi: true,
+            quotes: "double",
+        },
+    }, {
+        rules: {
+            "ts/consistent-type-definitions": ["error", "type"],
+            "no-console": ["warn"],
+            "antfu/no-top-level-await": ["off"],
+            "node/prefer-global/process": ["off"],
+            "node/no-process-env": ["error"],
+            "unicorn/filename-case": ["error", {
+                case: "kebabCase",
+                ignore: ["README.md"],
+            }],
+        },
+    }),
+);
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
-  eslint: {
-    config: {
-      standalone: false
-    }
-  },
-  vite: {
-    optimizeDeps: {
-      include: [
-        '@vue/devtools-core',
-        '@vue/devtools-kit',
-      ]
-    }
-  }
+    compatibilityDate: "2025-07-15",
+    devtools: { enabled: true },
+    modules: ["@nuxt/eslint"],
+    eslint: {
+        config: {
+            standalone: false,
+        },
+    },
+    vite: {
+        optimizeDeps: {
+            include: [
+                "@vue/devtools-core",
+                "@vue/devtools-kit",
+            ],
+        },
+    },
 
-})
+});
