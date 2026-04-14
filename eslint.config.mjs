62454af232739100be52ca864ee8f738b5726e2e
Mohamed Mujahid <Mohamed.mojahead@gmail.com>
2026-04-14 23:30:57 +0200

    test linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,7 +19,6 @@ export default withNuxt(
       "antfu/no-top-level-await": ["off"],
       "node/prefer-global/process": ["off"],
       "node/no-process-env": ["error"],
-      "perfectionist/sort-imports": ["error", { tsconfig: { rootDir: "." } }],
       "unicorn/filename-case": ["error", {
         case: "kebabCase",
         ignore: ["README.md"]
